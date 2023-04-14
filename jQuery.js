1fb195ae260464cdba09aa0f1eafedf5dde55e4b
SureshP2002 <104820512+SureshP2002@users.noreply.github.com>
2023-04-14 13:47:16 +0530

    DONE

---
+++ jQuery.js
@@ -0,0 +1,39 @@
+<script>
+        // $(function(){
+        //    $('h1').css("color","red");
+        // })
+        // $('p').html("HELLO");
+        // //$('p').hide();
+        // $('p').show();
+        // $('p').fadeOut(3000);
+        // $('#btn').mousedown(()=>{
+        //     $('body').css("background-color","blue");
+
+        // })
+        // $('h1').hide();
+        // $('h1').fadeIn(3000).delay(200);
+        // $('h1').fadeOut(3000).delay(200)
+        // $(function(){
+        //     let count=0;
+        //     $('#btn').click(()=>{
+        //         count++;
+        //         $('p').html(`your score ${count}`);
+        //     })
+        // })
+        // $('#btn').click(()=>{
+        //     $('#ONE').empty();
+        // })
+        // $('th').html().append("welcome")
+        // $(document).ready(function(){
+//   $("#o").click(function(){
+//     $("div").animate({left: '1000px'}, 1000);
+//     $("div").animate({fontSize: '3em'}, 1000);
+//   });
+//   $("#t").click(function(){
+//     $("div").stop();
+//   });
+// })
+//   $('p').css("color","red").fadeIn(3000).delay(1000).slideUp();
+//   $('H1').append($('H1').val());
+       
+    </script>
